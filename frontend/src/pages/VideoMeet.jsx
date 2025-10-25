7878060ba5d607ac3e641d46405dc46e7d7aaaab
Yash Singh <yashsingh93804@gmail.com>
2025-10-25 15:07:29 +0530

    push static.json file

--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -11,12 +11,12 @@ import MicOffIcon from '@mui/icons-material/MicOff'
 import ScreenShareIcon from '@mui/icons-material/ScreenShare';
 import StopScreenShareIcon from '@mui/icons-material/StopScreenShare'
 import ChatIcon from '@mui/icons-material/Chat'
-
 import API_URL from '../environment';
 
 // const server_url = "http://localhost:8000";   // these is the backend connection on the local system 
 
 const server_url = API_URL;    // These is the connection of the environment variable 
+
 
 var connections = {};
 
